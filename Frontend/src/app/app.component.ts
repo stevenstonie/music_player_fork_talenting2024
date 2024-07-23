9d96c49f37dc65cd910c29ff0c2d239d8cd558b4
Tret Elena <elena.tret@student.unitbv.ro>
2024-07-23 17:32:48 +0300

    added the playbar (without functionality)

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlaybarComponent } from './components/playbar/playbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlaybarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Frontend';
