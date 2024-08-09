5a2b687873044837186bbe61636340a98222a749
stevenstonie <petreastefann@gmail.com>
2024-08-09 09:22:59 +0300

    created an album service test class, intialized it and added a happy path test

---
+++ backend.Tests/services/AlbumServiceTest.cs
@@ -0,0 +1,75 @@
+
+using backend.models;
+using backend.services;
+using backend.services.interfaces;
+using Moq;
+
+namespace backend.Tests.services
+{
+	public class AlbumServiceTest
+	{
+		private readonly AlbumService _albumService;
+
+		private readonly Mock<ICacheService> _cacheServiceMock;
+
+		public AlbumServiceTest()
+		{
+			_cacheServiceMock = new();
+
+			_cacheServiceMock.Setup(cacheService => cacheService.GetCachedSongs()).Returns(ReturnTestSongs());
+
+			_albumService = new(_cacheServiceMock.Object);
+		}
+
+		[Theory]
+		[InlineData("Album 1")]
+		[InlineData("Album 2")]
+		public void GetAlbumSongs_ShouldReturnAllSongsByAlbum(string albumName)
+		{
+			IEnumerable<Song> songs = _albumService.GetAlbumSongs(albumName);
+
+			Assert.NotNull(songs);
+			if (albumName == "Album 1") Assert.Equal(2, songs.Count());
+			if (albumName == "Album 2") Assert.Single(songs);
+
+			Assert.All(songs, song => Assert.Equal(albumName, song.Album));
+
+			_cacheServiceMock.Verify(cacheService => cacheService.GetCachedSongs(), Times.Exactly(1));
+		}
+
+		// ------------------------------------------------------------------------
+
+		private static List<Song> ReturnTestSongs()
+		{
+			return [
+				new Song
+				{
+					FileName = "file_name_1.mp3",
+					Title="Title 1",
+					Album="Album 1",
+					CreationDate=new DateTime(2022, 1, 1, 0, 0,0 , DateTimeKind.Utc),
+					Artist="Artist 1",
+					Duration = 64
+				},
+				new Song
+				{
+					FileName = "file_name_2.mp3",
+					Title="Title 2",
+					Album="Album 2",
+					CreationDate=new DateTime(2022, 1, 1, 0, 0,0 , DateTimeKind.Utc),
+					Artist="Artist 2",
+					Duration = 128
+				},
+				new Song{
+					FileName = "file_name_3.mp3",
+					Title="Title 3",
+					Album="Album 1",
+					CreationDate=new DateTime(2022, 1, 1, 0, 0,0 , DateTimeKind.Utc),
+					Artist="Artist 3",
+					Duration = 256
+				}
+			];
+		}
+
+	}
+}
